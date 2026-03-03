366e09ba6780acc6c75013ea28b6beaa53addc02
André Fortini <124889918+andrefrtn@users.noreply.github.com>
2026-03-03 14:51:45 -0300

    feat(food): adicionar entidade, DTOs, repositório e controller

---
+++ src/main/java/com/example/invit_in/controller/FoodController.java
@@ -0,0 +1,40 @@
+package com.example.invit_in.controller;
+
+import com.example.invit_in.food.Food;
+import com.example.invit_in.food.FoodRepository;
+import com.example.invit_in.food.FoodRequestDTO;
+import com.example.invit_in.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+
+    List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+    return foodList;
+    }
+
+
+}
---
+++ src/main/java/com/example/invit_in/food/FoodRequestDTO.java
@@ -0,0 +1,5 @@
+package com.example.invit_in.food;
+
+public record FoodRequestDTO(String title, String image, Integer price) {
+
+}
