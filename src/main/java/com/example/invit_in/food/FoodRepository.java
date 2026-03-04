206da4f40e95b02185c53a789fecb44870f57f49
André Fortini <124889918+andrefrtn@users.noreply.github.com>
2026-03-04 16:25:20 -0300

    Delete src directory

--- src/main/java/com/example/invit_in/food/FoodRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.invit_in.food;
-
-// Repositório responsável pelas operações de acesso ao banco de dados da entidade Food.
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoodRepository extends JpaRepository<Food, Long> {
-}
--- src/main/java/com/example/invit_in/food/FoodRequestDTO.java
+++
@@ -1,7 +0,0 @@
-package com.example.invit_in.food;
-
-// Representa os dados recebidos na requisição para criar ou atualizar um Food.
-
-public record FoodRequestDTO(String title, String image, Integer price) {
-
-}
